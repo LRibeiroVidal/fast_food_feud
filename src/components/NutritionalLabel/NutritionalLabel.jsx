c2dd8f7b539e23fd8be1cad780f68d3773adc7a2
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-06-20 19:53:39 -0700

    added the rest

--- src/components/NutritionalLabel/NutritionalLabel.jsx
+++ src/components/NutritionalLabel/NutritionalLabel.jsx
@@ -12,13 +12,10 @@ export function NutritionalLabel(props) {
       <h4 className="item-name">{props["nutritional-label"].item_name}</h4>
 
       <ul className="fact-list">
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].protein} fact-label={"protein"} fact-value={props["nutritional-label"].protein}/>
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].saturated_fat} fact-label={"saturated fat"} fact-value={props["nutritional-label"].saturated_fat}/>
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].sodium} fact-label={"sodium"} fact-value={props["nutritional-label"].sodium}/>
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].sugar} fact-label={"sugar"} fact-value={props["nutritional-label"].sugar}/>
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].total_fat} fact-label={"total fat"} fact-value={props["nutritional-label"].total_fat}/>
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].total_fat} fact-label={"total fat"} fact-value={props["nutritional-label"].total_fat}/>
-          <NutritionalLabelFact nutrition-fact={props["nutritional-label"].serving_size} fact-label={"serving size"} fact-value={props["nutritional-label"].serving_size}/>
+          {nutritionFacts.map((item) => {
+            console.log(item);
+            return <NutritionalLabelFact key={item.id} nutrition-fact={item} fact-label={item.label} fact-value={props["nutritional-label"][item.attribute]}/>
+          })}
       </ul>
     </div>
   )
@@ -26,12 +23,11 @@ export function NutritionalLabel(props) {
 
 export function NutritionalLabelFact(props) {
   console.log(props);
-  if (props["nutritonal-fact"] == null) return null;
 
   return (
     <li className="nutrition-fact">
-      <span className="fact-label">{props["nutritonal-fact"]["fact-label"]}</span>{" "}
-      <span className="fact-value">{props["nutritonal-fact"]["fact-value"]}</span>
+      <span className="fact-label">{props["fact-label"]}</span>{" "}
+      <span className="fact-value">{props["fact-value"]}</span>
     </li>
   )
 }
